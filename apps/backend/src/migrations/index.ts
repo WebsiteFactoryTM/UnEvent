76d5242a6a606c88a53e3a01ed0c5e6b941d58cf
Alex <office@websitefactory.ro>
2025-11-18 18:40:55 +0200

    removed old migrations

--- apps/backend/src/migrations/index.ts
+++ apps/backend/src/migrations/index.ts
@@ -1,9 +1,1 @@
-import * as migration_20251118_162727_initial_schema from './20251118_162727_initial_schema'
-
-export const migrations = [
-  {
-    up: migration_20251118_162727_initial_schema.up,
-    down: migration_20251118_162727_initial_schema.down,
-    name: '20251118_162727_initial_schema',
-  },
-]
+export const migrations = []
