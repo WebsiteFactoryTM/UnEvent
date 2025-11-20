cce239f0176ddd40b866ab8301b369cc0f972155
Alex <office@websitefactory.ro>
2025-11-20 07:59:51 +0200

    backend: fix migrations index formatting

--- apps/backend/src/migrations/index.ts
+++ apps/backend/src/migrations/index.ts
@@ -1,5 +1,5 @@
-import * as migration_20251118_191204_init from './20251118_191204_init';
-import * as migration_20251118_193644_add_prefix_to_media from './20251118_193644_add_prefix_to_media';
+import * as migration_20251118_191204_init from './20251118_191204_init'
+import * as migration_20251118_193644_add_prefix_to_media from './20251118_193644_add_prefix_to_media'
 
 export const migrations = [
   {
@@ -10,6 +10,6 @@ export const migrations = [
   {
     up: migration_20251118_193644_add_prefix_to_media.up,
     down: migration_20251118_193644_add_prefix_to_media.down,
-    name: '20251118_193644_add_prefix_to_media'
+    name: '20251118_193644_add_prefix_to_media',
   },
-];
+]
