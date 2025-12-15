0e85f2582b821535b143b6c28710b2992c369e0f
Alex <office@websitefactory.ro>
2025-12-15 14:57:15 +0200

    fixed migration error

--- apps/backend/src/migrations/index.ts
+++ apps/backend/src/migrations/index.ts
@@ -1,15 +1,14 @@
-import * as migration_20251118_191204_init from './20251118_191204_init';
-import * as migration_20251118_193644_add_prefix_to_media from './20251118_193644_add_prefix_to_media';
-import * as migration_20251123_143804_enable_api_keys_for_users from './20251123_143804_enable_api_keys_for_users';
-import * as migration_20251129_202656_add_ticket_url_to_events from './20251129_202656_add_ticket_url_to_events';
-import * as migration_20251202_102606 from './20251202_102606';
-import * as migration_20251204_121815 from './20251204_121815';
-import * as migration_20251204_125748 from './20251204_125748';
-import * as migration_20251204_135953 from './20251204_135953';
-import * as migration_20251205_075202 from './20251205_075202';
-import * as migration_20251211_100529_add_claim_status from './20251211_100529_add_claim_status';
-import * as migration_20251215_073929_make_city_optional_for_drafts from './20251215_073929_make_city_optional_for_drafts';
-import * as migration_20251215_124126 from './20251215_124126';
+import * as migration_20251118_191204_init from './20251118_191204_init'
+import * as migration_20251118_193644_add_prefix_to_media from './20251118_193644_add_prefix_to_media'
+import * as migration_20251123_143804_enable_api_keys_for_users from './20251123_143804_enable_api_keys_for_users'
+import * as migration_20251129_202656_add_ticket_url_to_events from './20251129_202656_add_ticket_url_to_events'
+import * as migration_20251202_102606 from './20251202_102606'
+import * as migration_20251204_121815 from './20251204_121815'
+import * as migration_20251204_125748 from './20251204_125748'
+import * as migration_20251204_135953 from './20251204_135953'
+import * as migration_20251205_075202 from './20251205_075202'
+import * as migration_20251211_100529_add_claim_status from './20251211_100529_add_claim_status'
+import * as migration_20251215_073929_make_city_optional_for_drafts from './20251215_073929_make_city_optional_for_drafts'
 
 export const migrations = [
   {
@@ -67,9 +66,4 @@ export const migrations = [
     down: migration_20251215_073929_make_city_optional_for_drafts.down,
     name: '20251215_073929_make_city_optional_for_drafts',
   },
-  {
-    up: migration_20251215_124126.up,
-    down: migration_20251215_124126.down,
-    name: '20251215_124126'
-  },
-];
+]
